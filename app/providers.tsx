1cb8873253fb3d486a170862b89f310e5f0fcd8d
krish <kishux99@gmail.com>
2026-08-09 14:29:21 +0530

    Update all modified files on ShopVibee

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,24 @@
-'use client';
+'use client'
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import React from 'react'
+import NextTopLoader from 'nextjs-toploader'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <>
+      {/* Yeh raha aapka smooth yellow loading bar */}
+      <NextTopLoader 
+        color="#EAB308" 
+        initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={false}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #EAB308,0 0 5px #EAB308"
+      />
       {children}
-    </NextThemesProvider>
-  );
+    </>
+  )
 }
